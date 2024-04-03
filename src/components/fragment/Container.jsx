96e0da5da38074e9c2572a633a0844eb1e6a50ff
Vaniarumuam17 <vani00712@gmail.com>
2024-04-03 23:09:03 +0530

    first-commit

---
+++ src/components/fragment/Container.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../assets/scss/Container.scss";
+
+const Container = ({ children }) => {
+  return <div className={"Container"}>{children}</div>;
+};
+
+export default Container;
